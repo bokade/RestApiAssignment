089237acfc072008d03850adc97d1180b6900776
Swapnil Bokade <121025630+bokade@users.noreply.github.com>
2023-08-21 10:47:00 +0530

    Create RestApiAssignmentApplication.java

---
+++ in.sp/RestApiAssignmentApplication.java
@@ -0,0 +1,13 @@
+package in.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiAssignmentApplication.class, args);
+	}
+
+}
